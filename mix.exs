e17014341d0d0c5fefed4434bf948a2faac43897
Aaron Bull Schaefer <aaron@elasticdog.com>
2016-12-29 22:20:23 -0800

    Convert from umbrella app back into standalone library

--- mix.exs
+++ mix.exs
@@ -5,10 +5,6 @@ defmodule Socket.Mixfile do
     [
       app: :socket,
       version: "0.1.0",
-      build_path: "../../_build",
-      config_path: "../../config/config.exs",
-      deps_path: "../../deps",
-      lockfile: "../../mix.lock",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
